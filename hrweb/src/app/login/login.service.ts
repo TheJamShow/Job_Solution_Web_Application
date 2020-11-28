408d628965f39ba8798668f08d28913684687fa1
Jay Shah <jayjshah2000@gmail.com>
2020-11-28 21:14:13 +0530

    final

--- hrweb/src/app/login/login.service.ts
+++ hrweb/src/app/login/login.service.ts
@@ -73,7 +73,6 @@ export class LoginService {
                 "http://localhost:3000/login",
                 // "login",
                 authData
-<<<<<<< HEAD
             )
             .subscribe(response => {
                 console.log(response);
@@ -110,56 +109,6 @@ export class LoginService {
                 return false;
             });
             return false;
-=======
-            ).toPromise()
-        if(resp){
-            console.log(resp);
-            return(this.getAuthValid(resp));
-
-        }
-        else{
-            return false;
-        }
-    
-            // .subscribe(response => {
-            //     console.log("response",response);
-            //     const token = response.token;
-            //     this.token = token;
-            //     console.log(token);
-            //     if (token) {
-            //         const expiresInDuration = response.expiresIn;
-            //         this.setAuthTimer(expiresInDuration);
-            //         this.isAuthenticated = true;
-            //         this.userId = response.userId;
-            //         this.userRole = response.userRole;
-            //         this.authStatusListener.next(true);
-            //         const now = new Date();
-            //         const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
-            //         console.log("role: " ,this.userRole);
-            //         console.log("token: " ,token);
-            //         this.saveAuthData(token, expirationDate, this.userId);
-            //         console.log(this.userRole);
-            //         if(this.userRole == 'HR'){
-            //             this.router.navigate(["/view-posting"]);
-            //         }
-            //         else if (this.userRole == 'Candidate'){
-            //             this.router.navigate(["/jobspage"]);
-            //         }
-            //         else{
-            //             return false;
-            //         }
-            //     }
-            //     else{
-            //         return false;
-            //     }
-            // },err => {
-            //     console.log('err');
-            //     return false;
-            // });
-
-            // console.log('true');
-            // return true;
->>>>>>> 418bdabc94bf83306bd67f1a251950a30b6b48ee
     }
 
     getUserEmail() {
